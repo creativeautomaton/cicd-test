fc4ad4151c4ab1ac1aaa5eefae471525818a7e78
Zachary Kraft <creativeautomaton@users.noreply.github.com>
2022-06-15 16:07:22 -0500

    new strapi image/ using aws ecr remote repo

---
+++ strapi/config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'postgres'),
+        port: env.int('DATABASE_PORT', 5433),
+        database: env('DATABASE_NAME', 'db_strapi'),
+        username: env('DATABASE_USERNAME', 'my_user'),
+        password: env('DATABASE_PASSWORD', 'my_root_password'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9921ee11-8b52-4ec8-a73c-3c7cbe988b3b'
+};
