03a853cea1cdb84879acd49c3dd7f07f7cbb3883
Zachary Kraft <creativeautomaton@users.noreply.github.com>
2022-06-15 15:28:37 -0500

    new strapi image/ using aws ecr remote repo

---
+++ cms/srv/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '74ee65408c763d732c31922aa272943d'),
+    },
+  },
+});
